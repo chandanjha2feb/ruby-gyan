8e70b747819a65e0f0b9cb7c4b82bb26a51bc18a
chandanjha2feb <chandanjha2feb@gmail.com>
2024-04-14 12:06:30 +0200

    fix lessons urls

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -14,6 +14,7 @@ class LessonsController < ApplicationController
   # GET /lessons/new
   def new
     @lesson = Lesson.new
+    @course = Course.friendly.find(params[:course_id])
   end
 
   # GET /lessons/1/edit
@@ -23,10 +24,11 @@ class LessonsController < ApplicationController
 
   # POST /lessons or /lessons.json
   def create
-    @lesson = Lesson.new(lesson_params)
+    @course = Course.friendly.find(params[:course_id])
+    @lesson = @course.lessons.new(lesson_params)
     respond_to do |format|
       if @lesson.save
-        format.html { redirect_to lesson_url(@lesson), notice: "Lesson was successfully created." }
+        format.html { redirect_to course_lesson_path(@course, @lesson), notice: "Lesson was successfully created." }
         format.json { render :show, status: :created, location: @lesson }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +42,7 @@ class LessonsController < ApplicationController
     authorize @lesson
     respond_to do |format|
       if @lesson.update(lesson_params)
-        format.html { redirect_to lesson_url(@lesson), notice: "Lesson was successfully updated." }
+        format.html { redirect_to course_lesson_path(@course, @lesson), notice: "Lesson was successfully updated." }
         format.json { render :show, status: :ok, location: @lesson }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -55,7 +57,7 @@ class LessonsController < ApplicationController
     @lesson.destroy
 
     respond_to do |format|
-      format.html { redirect_to lessons_url, notice: "Lesson was successfully destroyed." }
+      format.html { redirect_to courses_lesson_path(@course), notice: "Lesson was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -63,6 +65,7 @@ class LessonsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_lesson
+      @course = Course.friendly.find(params[:course_id])
       @lesson = Lesson.friendly.find(params[:id])
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :lessons
   devise_for :users
-  resources :courses
+  resources :courses do
+    resources :lessons
+  end
   resources :users
   root 'home#index'
   get 'home/activities'
