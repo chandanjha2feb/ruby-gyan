c2cb26603282e39fb8169222aac812d73eff1efb
chandanjha2feb <chandanjha2feb@gmail.com>
2024-05-06 23:26:21 +0200

    move queries to scope

--- app/models/course.rb
+++ app/models/course.rb
@@ -18,6 +18,11 @@ class Course < ApplicationRecord
     LANGUAGES = [:"English", :"Russian", :"Polish", :"Spanish"]
     LEVELS = [:"Beginner", :"Intermediate", :"Advanced"]
 
+    scope :latest, -> { limit(3).order(created_at: :desc) }
+    scope :top_rated, -> { limit(3).order(average_rating: :desc, created_at: :desc) }
+    scope :popular, -> { limit(3).order(enrollments_count: :desc, created_at: :desc) }
+
+
     def self.languages
       LANGUAGES.map { |language| [language, language] }
     end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -16,6 +16,8 @@ class Enrollment < ApplicationRecord
 
   scope :pending_review, -> { where(rating: [0, nil, ""], review: [0, nil, ""]) }
   scope :reviewed, -> { where.not(review: [0, nil, ""]) }
+  scope :latest_good_reviews, -> { order(rating: :desc, created_at: :desc).limit(3) }
+
 
   after_save do
     unless rating.nil? || rating.zero?
