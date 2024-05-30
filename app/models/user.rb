0a9e1a8c9bfccd71c347a978fbff8c4f231f0e70
chandanjha2feb <chandanjha2feb@gmail.com>
2024-05-30 10:07:30 +0200

    validations with destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   rolify
   resourcify
   
-  has_many :courses, dependent: :destroy
-  has_many :enrollments
-  has_many :user_lessons
+  has_many :courses, dependent: :nullify
+  has_many :enrollments, dependent: :nullify
+  has_many :user_lessons, dependent: :nullify
 
   after_create :assign_default_role
 
--- app/models/user_lesson.rb
+++ app/models/user_lesson.rb
@@ -1,5 +1,4 @@
 class UserLesson < ApplicationRecord
-
     belongs_to :user
     belongs_to :lesson
 
