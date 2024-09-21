7e9da874fb7f6f1cbff27f7ff91fc85587a11548
chandanjha2feb <chandanjha2feb@gmail.com>
2024-09-21 09:17:10 +0200

    Added some styling and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 # user.save!
 PublicActivity.enabled = false
 10.times do
-    Course.create!([{
+    course = Course.new(
         title: Faker::Educator.course_name,
         description: Faker::TvShows::GameOfThrones.quote,
         user_id: User.first.id,
@@ -15,7 +15,8 @@ PublicActivity.enabled = false
         language: Faker::ProgrammingLanguage.name,
         level: "Beginner",
         price: Faker::Number.between(from: 1000, to: 20000)
-    }])
+    )
+    course.save(validate: false)
 end
 PublicActivity.enabled = true
 
