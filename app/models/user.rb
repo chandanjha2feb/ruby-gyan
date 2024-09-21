a0d983057ef97ea6c66d93eba7fee43616b776ac
chandanjha2feb <chandanjha2feb@gmail.com>
2024-09-21 23:01:39 +0200

    Add facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable,
-         :omniauthable, omniauth_providers: [:google_oauth2, :github]
+         :omniauthable, omniauth_providers: [:google_oauth2, :github, :facebook]
 
   extend FriendlyId
   friendly_id :email, use: :slugged
