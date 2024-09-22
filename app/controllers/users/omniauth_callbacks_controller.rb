1687aae0eef7b7ef2d0947a5b174d9632d1e5617
chandanjha2feb <chandanjha2feb@gmail.com>
2024-09-22 18:11:53 +0200

    refractor code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,33 +1,26 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-    end
+    handle_auth("google")
   end
 
   def github
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Github'
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session['devise.github_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-    end
+    handle_auth("github")
   end
 
   def facebook
+    handle_auth("facebook")
+  end
+
+  private
+
+  def handle_auth(kind)
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook'
+      UserMailer.new_user(@user).deliver_later
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
       sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      session["devise.#{kind}_data"] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
